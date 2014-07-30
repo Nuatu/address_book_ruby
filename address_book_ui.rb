e8ed577c7495633c82c3c715b71225f7a2f14c82
Christian Danielsen & Nuatu Tseggai <student@epicodus.com>
2014-07-30 16:38:12 -0700

    Create delete contact attributes functionality

--- address_book_ui.rb
+++ address_book_ui.rb
@@ -18,10 +18,13 @@ def main_menu
 end
 
 def contact_editor(input)
- puts "\nYou're modifiying the contact: #{Contact.all[input - 1].name}"
+ puts "\nYou're modifiying the contact: #{Contact.all[input - 1].name} \n Phone: #{Contact.all[input - 1].phone} Email: #{Contact.all[input - 1].email} Address: #{Contact.all[input - 1].address}"
   puts "\nPress '1' to add an email address"
   puts "Press '2' to add a phone number"
   puts "Press '3' to add an address"
+  puts "Press '4' to delete an email address"
+  puts "Press '5' to delete an phone number"
+  puts "Press '6' to delete an address"
   puts "Press 'm' to return to main menu"
   editing_choice = gets.chomp.to_i
   # unless editing_choice != '1' and editing_choice != '2' and editing_choice != '3'
@@ -38,11 +41,31 @@ def contact_editor(input)
       Contact.all[input-1].phone << new_phone
       contact_editor(input)
 
-
     elsif editing_choice == 3
       puts "Address?"
       new_address = gets.chomp
       Contact.all[input-1].address << new_address
+      contact_editor(input)
+
+    elsif editing_choice == 4
+      puts "Email address to delete?"
+      Contact.all[input-1].email.each_with_index { | email, index | puts "#{index + 1}. #{email}" }
+      deleted_email = gets.chomp.to_i
+      Contact.all[input-1].email.delete_at(deleted_email-1)
+      contact_editor(input)
+
+    elsif editing_choice == 5
+      puts "Phone to delete?"
+      Contact.all[input-1].phone.each_with_index { | phone, index | puts "#{index + 1}. #{phone}" }
+      deleted_phone = gets.chomp.to_i
+      Contact.all[input-1].phone.delete_at(deleted_phone-1)
+      contact_editor(input)
+
+    elsif editing_choice == 6
+      puts "Address to delete?"
+      Contact.all[input-1].address.each_with_index { | address, index | puts "#{index + 1}. #{address}" }
+      deleted_address = gets.chomp.to_i
+      Contact.all[input-1].address.delete_at(deleted_address-1)
       contact_editor(input)
     end
   end
