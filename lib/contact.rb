8573c63574f6d892e3ec8e59e68fc52b8708aef6
Christian Danielsen & Nuatu Tseggai <student@epicodus.com>
2014-07-30 10:26:04 -0700

    Create contact class with name/email/phone/address

---
+++ lib/contact.rb
@@ -0,0 +1,26 @@
+class Contact
+  def initialize(name)
+    @name = name
+    @emails = []
+    @phones = []
+    @addresses = []
+  end
+
+  def name
+    @name
+  end
+
+  def emails
+    @emails
+  end
+
+  def phones
+    @phones
+  end
+
+  def addresses
+    @addresses
+  end
+end
+
+
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,12 +1,23 @@
 require 'rspec'
 require 'address_book'
+require 'contact'
 
 describe 'AddressBook' do
-  it 'creates a contact object' do
+  it 'creates an address book object' do
     test_book = AddressBook.new('X')
     expect(test_book).to be_an_instance_of AddressBook
+    expect test_book.description == 'X'
     expect(test_book.contacts).to eq []
   end
-
 end
 
+describe 'Contact' do
+  it 'creates a contact object' do
+    test_contact = Contact.new('Y')
+    expect(test_contact).to be_an_instance_of Contact
+    expect test_contact.name == 'Y'
+    expect(test_contact.emails).to eq []
+    expect(test_contact.phones).to eq []
+    expect(test_contact.addresses).to eq []
+  end
+end
