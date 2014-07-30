082a4e61dcf056dcae33930eed571f8d58d0d424
Christian Danielsen & Nuatu Tseggai <student@epicodus.com>
2014-07-30 15:55:10 -0700

    Update UI options / flow

--- address_book_ui.rb
+++ address_book_ui.rb
@@ -7,7 +7,7 @@ require './lib/address'
 def main_menu
   loop do
     #Main menu block
-    puts "\n\nWelcome to AddressBook by Ruby"
+    puts "\nWelcome to AddressBook by Ruby"
     puts "Press '1' to create a new contact"
     puts "Press '2' to view/edit existing contacts"
     puts "Press 'any other key' to quit"
@@ -18,7 +18,34 @@ def main_menu
 end
 
 def contact_editor(input)
-  puts "Which contact to modify?"
+ puts "\nYou're modifiying the contact: #{Contact.all[input - 1].name}"
+  puts "\nPress '1' to add an email address"
+  puts "Press '2' to add a phone number"
+  puts "Press '3' to add an address"
+  puts "Press 'm' to return to main menu"
+  editing_choice = gets.chomp.to_i
+  # unless editing_choice != '1' and editing_choice != '2' and editing_choice != '3'
+  unless (editing_choice == 'm')
+    if editing_choice == 1
+      puts "Email address?"
+      new_email = gets.chomp
+      Contact.all[input-1].email << new_email
+      contact_editor(input)
+
+    elsif editing_choice == 2
+      puts "Phone?"
+      new_phone = gets.chomp
+      Contact.all[input-1].phone << new_phone
+      contact_editor(input)
+
+
+    elsif editing_choice == 3
+      puts "Address?"
+      new_address = gets.chomp
+      Contact.all[input-1].address << new_address
+      contact_editor(input)
+    end
+  end
 end
 
 def main_menu_selector(input)
@@ -29,19 +56,12 @@ def main_menu_selector(input)
     newContact.save
   elsif input == '2'
     puts "Existing Contacts:"
-    Contact.all.each_with_index { |contact, index| puts "#{index + 1}. #{contact.name}"}
+    Contact.all.each_with_index { |contact, index| puts "#{index + 1}. #{contact.name} \n Phone: #{contact.phone} Email: #{contact.email} Address: #{contact.address}"}
     puts "Enter a contact number to modify or press 'm' to exit"
     modify_choice = gets.chomp.to_i
-    exit if modify_choice == 'm'
-
-    puts "\n\nYou're modifiying the contact: #{Contact.all[modify_choice - 1].name}"
-    puts "Press '1' to add an email address"
-    puts "Press '2' to add a phone number"
-    puts "Press '3' to add an address"
-    puts "Press 'm' to return to main menu"
-    editing_choice = gets.chomp
-    exit if editing_choice != '1' and editing_choice != '2' and editing_choice != '3'
-    contact_editor(editing_choice)
+    unless modify_choice == 'm'
+      contact_editor(modify_choice)
+    end
   end
 end
 
